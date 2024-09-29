52e562463b82e102a559f2f836adbe34684ae626
ch55secake <oscardj112@gmail.com>
2024-09-29 15:52:00 +0100

    update main.go to include http client, sending request to the server that I start

--- main.go
+++ main.go
@@ -5,25 +5,29 @@ import (
 	"github.com/sirupsen/logrus"
 	"http-in-go/httpserver"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func main() {
 	httpserver.CreateHTTPServer("8080")
 
-	requestUrl := fmt.Sprintf("http://localhost:%s", "8080")
-	res, err := http.Get(requestUrl)
+	c := http.Client{Timeout: time.Duration(1) * time.Second}
+	resp, err := c.Get("http://localhost:8080")
 	if err != nil {
-		logrus.Errorf("Error making http request: %s", err)
+		fmt.Printf("Error %s", err)
+		return
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
 
 		}
-	}(res.Body)
+	}(resp.Body)
+	_, err = ioutil.ReadAll(resp.Body)
 
 	logrus.Info("Client: got response!\n")
-	logrus.Infof("Client: response status code: %v\n", res.StatusCode)
+	logrus.Infof("Client: response status code: %v\n", resp.StatusCode)
 
 }
